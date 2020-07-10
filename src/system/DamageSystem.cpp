61cd4d42b4faaff39cce11241e55232901aa418a
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-07-10 05:07:38 +0300

    fix reference

--- src/system/DamageSystem.cpp
+++ src/system/DamageSystem.cpp
@@ -5,12 +5,12 @@ namespace RoguesParty {
   void DamageSystem::receive(const DamageEvent &damageEvent) {
     int resultingDamage = damageEvent.damage;
     /// TODO: add resists and other effects handling
-    entityx::Entity &target = damageEvent.damage;
+    auto target = damageEvent.target;
     if (target.has_component<Damageable>()) {
-      auto &damagable = damageEvent.target.component<Damageable>();
-      damagable.hp -= resultingDamage;
-      if (damagable.hp <= 0)
-        damagable.dead = true;
+      auto damagable = target.component<Damageable>();
+      damagable->hp -= resultingDamage;
+      if (damagable->hp <= 0)
+        damagable->dead = true;
     }
   }
 }
