f6da808e7e0a348c4d217c061f9241b8f8a6fb09
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-07-12 10:49:54 +0300

    add data storeing

--- src/core/Tile.cpp
+++ src/core/Tile.cpp
@@ -1,2 +1,36 @@
 #include "Tile.h"
+#include "../component/Object.h"
 
+namespace RoguesParty {
+  void Tile::add(std::shared_ptr<entityx::Entity> object) {
+    auto volume = object->component<Size>()->volume;
+    auto i = objects.cbegin();
+    for (; i != objects.cend(); ++i) {
+      if (**i == *object)
+        return;
+      if ((*i)->component<Size>()->volume > volume) 
+        break;
+    }
+    objects.insert(i, object);
+  }
+
+  const std::list<std::shared_ptr<entityx::Entity>> &Tile::getEntities() {
+    return objects;
+  }
+
+  nlohmann::json Tile::getData() {
+    std::list<nlohmann::json> result;
+    for (auto i = objects.cbegin(); i != objects.cend(); ++i) {
+      /// TODO: add more object properties
+      result.emplace_back(nlohmann::json {
+          {"type", (*i)->component<Object>()->type},
+          {"size", (*i)->component<Size>()->volume},
+          {"effects", {}}
+          });
+    }
+    return nlohmann::json {
+      {"type", type},
+        {"objects", result}
+    };
+  }
+}
--- src/core/Tile.h
+++ src/core/Tile.h
@@ -1,9 +1,31 @@
 #ifndef TILE_H
 #define TILE_H
+#include <list>
+#include <memory>
+#include "entityx/Entity.h"
+#include "../component/Size.h"
+#include "nlohmann/json.hpp"
+#include "TileType.h"
 
 namespace RoguesParty {
   /** Contains map cell propertys */
   class Tile {
+    private:
+      /// contains entities placed on this tile ordered by size
+      /// TODO: replace with boost::flat_set
+      std::list<std::shared_ptr<entityx::Entity>> objects;
+      TileType type;
+
+    public:
+      /** adds entity to the tile */
+      void add(std::shared_ptr<entityx::Entity> object);
+
+      /** get objects on this tile */
+      const std::list<std::shared_ptr<entityx::Entity>> &getEntities();
+
+      /** get objects on this tile with some data may be needed to draw it on 
+       * map */
+      nlohmann::json getData();
   };
 }
 
