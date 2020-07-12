aa5c5b86c33092337f7efe46a79e89c30a163ad5
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-07-12 12:09:12 +0300

    change TileType to Tile

--- src/core/Map.h
+++ src/core/Map.h
@@ -6,6 +6,7 @@
 #include "../core/TileType.h"
 #include "nlohmann/json.hpp"
 #include "../core/Coordinate.h"
+#include "../core/Tile.h"
 
 namespace RoguesParty {
   /** Contains map data. Handles object positioning on map */
@@ -13,17 +14,17 @@ namespace RoguesParty {
     friend class MapChangeSystem;
     friend class Game;
     private:
-      std::vector<std::vector<TileType>> map;
-      std::vector<std::vector<TileType>> changedMap;
-      std::vector<std::vector<TileType>> previousMap;
+      std::vector<std::vector<Tile>> map;
+      std::vector<std::vector<Tile>> changedMap;
+      std::vector<std::vector<Tile>> previousMap;
 
     public:
       Map() {
         int mapXSize, mapYSize;
         mapXSize = mapYSize = 50;
-        map.resize(mapYSize, std::vector<TileType>(mapXSize, TileType::GROUND));
-        changedMap.resize(mapYSize, std::vector<TileType>(mapXSize, TileType::GROUND));
-        previousMap.resize(mapYSize, std::vector<TileType>(mapXSize, TileType::GROUND));
+        map.resize(mapYSize, std::vector<Tile>(mapXSize, TileType::GROUND));
+        changedMap.resize(mapYSize, std::vector<Tile>(mapXSize, TileType::GROUND));
+        previousMap.resize(mapYSize, std::vector<Tile>(mapXSize, TileType::GROUND));
         map[0][0] = changedMap[0][0] = previousMap[0][0] = TileType::HUMAN;
       }
 
