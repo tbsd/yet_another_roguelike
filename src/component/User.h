5751efd6ecb1d478457fe44b43780ae25ca9a186
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-07-12 04:56:46 +0300

    add map cache for users

--- src/component/User.h
+++ src/component/User.h
@@ -2,16 +2,24 @@
 #define USER_H
 #include <cstdint>
 #include <string>
+#include <vector>
+#include "../core/TileType.h"
+#include "../core/Coordinate.h"
 
 namespace RoguesParty {
+  /** User unique identifier for current session */
+  using Id = std::string; 
+
   /** Contains player data */
   struct User {
-    /** User unique identifier for current session */
-    using Id = std::string; 
 
     Id id;
+    /// Map data that user already have
+    std::vector<std::vector<TileType>> mapCache;
 
-    User (Id id) : id(id) {}
+    User (Id id, Coordinate mapXSize = 50, Coordinate mapYSize = 50) : id(id) {
+      mapCache.resize(mapYSize, std::vector<TileType>(mapXSize, TileType::GROUND));
+    }
   };
 }
 
