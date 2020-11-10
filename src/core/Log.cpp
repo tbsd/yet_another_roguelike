be827c35a0e55b7f327b29731bd896043e12abc0
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-11-10 20:29:37 +0300

    add logger

---
+++ src/core/Log.cpp
@@ -0,0 +1,14 @@
+//
+// Created by subject on 11/10/20.
+//
+
+#include "Log.h"
+#include <iostream>
+
+namespace tbsd {
+  const char* const Log::messageLabel[] = {"Message", "Warning", "Error", "FatalError"};
+
+  void Log::send(std::string_view msg, Log::Type type) {
+    std::cout << "[" << messageLabel[type] << "] " << msg << std::endl;
+  }
+}
---
+++ src/core/Log.h
@@ -0,0 +1,18 @@
+#ifndef TBSD_ROGUELIKE_LOGGER_H
+#define TBSD_ROGUELIKE_LOGGER_H
+#include <string_view>
+
+namespace tbsd {
+  class Log {
+  public:
+    enum Type {
+      Message, Warning, Error, FatalError
+    };
+  private:
+    static const char *const messageLabel[];
+  public:
+    static void send(std::string_view msg, Type type = Type::Message);
+  };
+}
+
+#endif //TBSD_ROGUELIKE_LOGGER_H
