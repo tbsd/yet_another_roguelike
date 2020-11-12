e93e0e57704c377329d81d471263b58d44182f7d
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-11-12 15:38:06 +0300

    add receiving console input form server

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -1,10 +1,23 @@
 #include "Game.h"
 #include "Log.h"
 #include <mutex>
+#include <future>
+#include <chrono>
+#include "UserAction.h"
 
 namespace tbsd {
   void Game::run() {
+    std::string consoleInput;
+    std::future<std::string> console = std::async([&]{return io.getFromConsole();});
+    // Main game loop
     while (true) {
+      // Process data from console
+      if (console.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+        consoleInput = console.get();
+        Log::send("Received command: " + consoleInput);
+        console = std::async([&]{return io.getFromConsole();});
+      }
+      // Process data from server
       std::mutex m;
       m.lock();
       if (server.hasUserActions()) {
@@ -16,4 +29,5 @@ namespace tbsd {
         m.unlock();
     }
   }
+
 }
--- src/core/Game.h
+++ src/core/Game.h
@@ -1,11 +1,13 @@
 #ifndef TBSD_ROGUELIKE_GAME_H
 #define TBSD_ROGUELIKE_GAME_H
 #include "Server.h"
+#include "IO.h"
 
 namespace tbsd {
   /// Contains general information about game and handles reaction to user actions
   class Game {
     Server& server;
+    IO io;
 
   public:
     explicit Game(Server& server) : server(server){}
