7d5f4d6ad1f8f81b0ad91fd4d7b84995e8fda942
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-11-12 17:30:18 +0300

    add UserAction

---
+++ src/core/UserAction.h
@@ -0,0 +1,12 @@
+#ifndef TBSD_ROGUELIKE_USERACTION_H
+#define TBSD_ROGUELIKE_USERACTION_H
+#include <string>
+#include <server/ws/ws_session.h>
+
+
+namespace tbsd {
+  using UserAction = std::string;
+}
+
+
+#endif //TBSD_ROGUELIKE_USERACTION_H
