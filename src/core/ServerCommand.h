d91356880de8dd8139e48813654f20f706d13864
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-11-13 11:36:27 +0300

    add ServerCommand

---
+++ src/core/ServerCommand.h
@@ -0,0 +1,32 @@
+//
+// Created by subject on 11/13/20.
+//
+
+#ifndef TBSD_ROGUELIKE_SERVERCOMMANDS_H
+#define TBSD_ROGUELIKE_SERVERCOMMANDS_H
+#include <map>
+
+
+namespace tbsd {
+  namespace {
+    struct strLess {
+      bool operator()(const char *lhs, const char *rhs) const {
+        return std::strcmp(lhs, rhs) < 0;
+      }
+    };
+  }
+
+/// Commands from server command line
+  enum ServerCommand {
+    Shutdown,
+    Invalid
+  };
+
+/// Contains mapping for commands from server command line
+const std::map<const char *, ServerCommand, strLess> CommandsMap {
+      {"shut", Shutdown}
+};
+}
+
+
+#endif //TBSD_ROGUELIKE_SERVERCOMMANDS_H
