b8d3911418c6a6c8bfbf0240c6678e4a4509bac5
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-11-23 14:22:12 +0300

    add entityx managers

--- src/core/Game.h
+++ src/core/Game.h
@@ -3,14 +3,18 @@
 #include "Server.h"
 #include "IO.h"
 #include "ServerCommand.h"
+#include "entityx/entityx.h"
 
 namespace tbsd {
   /// Contains general information about game and handles reaction to user actions
   class Game {
     Server& server;
+    entityx::EventManager events;
+    entityx::EntityManager entities;
+    entityx::SystemManager systems;
 
   public:
-    explicit Game(Server& server) : server(server){}
+    explicit Game(Server& server) : server(server), entities(events), systems(entities, events) {}
 
     /// Starts main game loop
     void run();
