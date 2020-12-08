3ffcad22b9dca7cb54823939fb614bc9266e3e46
Anton Kurbatov <a.kurbatov@protonmail.com>
2020-12-08 23:48:33 +0300

    add operator overload

--- src/component/Position.h
+++ src/component/Position.h
@@ -15,6 +15,30 @@ struct Position {
   Coordinate z;
 
   Position(Coordinate x, Coordinate y, Coordinate z): x(x), y(y), z(z) {}
+
+  Position& operator+=(const Position& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    return *this;
+  }
+
+  friend Position operator+(Position& lhs, Position& rhs) {
+    return Position(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+  }
+
+  Position operator-() const {
+    return Position(-this.x, -this.y, -this.z);
+  }
+
+  Position& operator-=(const Position& lhs) {
+    operator+=(-lhs);
+    return *this;
+  }
+
+  friend Position operator-(Position& lhs, Position& rhs) {
+    return Position(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+  }
 };
 
 
